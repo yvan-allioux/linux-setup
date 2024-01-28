c1366d4ca0c8f1bfc1634a15d32b4071bb8d5a3e
yvan-allioux <yvan.allioux.pro@gmail.com>
2024-01-28 17:43:31 +0100

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#sudo apt update ; sudo apt upgrade -y ; sudo apt install git vim curl zsh -y
+
 sudo apt update
 sudo apt upgrade -y
 
@@ -51,10 +53,13 @@ sudo apt install grub-customizer -y
 #lazydocker       https://github.com/jesseduffield/lazydocker/releases/
 
 #wget https://github.com/jesseduffield/lazydocker/releases/download/v0.21.0/lazydocker_0.21.0_Linux_x86_64.tar.gz
+#wget https://github.com/jesseduffield/lazydocker/releases/download/v0.23.1/lazydocker_0.23.1_Linux_x86_64.tar.gz
 #tar -xvf lazydocker_*.tar.gz
 #sudo mv ./lazydocker /usr/bin
 #chmod +x /usr/bin/lazydocker
 #rm lazydocker_*.tar.gz LICENSE README.md
+
+#wget https://github.com/jesseduffield/lazydocker/releases/download/v0.23.1/lazydocker_0.23.1_Linux_x86_64.tar.gz && tar -xvf lazydocker_*.tar.gz && sudo mv ./lazydocker /usr/bin && chmod +x /usr/bin/lazydocker && rm lazydocker_*.tar.gz LICENSE README.md
 
 
 #docker
@@ -67,6 +72,8 @@ sudo apt install docker-ce -y
 sudo usermod -aG docker ${USER}
 sudo su - ${USER}
 #docker --version
+
+#sudo apt update && sudo apt install apt-transport-https ca-certificates curl software-properties-common -y && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && apt-cache policy docker-ce && sudo apt install docker-ce -y && sudo usermod -aG docker ${USER} && sudo su - ${USER}
 
 #Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
