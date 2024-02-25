9228f6bcfe445301cc02d0d74b7d0c8e405fba18
yvan-allioux <yvan.allioux.pro@gmail.com>
2024-02-25 01:02:45 +0100

    Create choco.ps1

---
+++ choco.ps1
@@ -0,0 +1,4 @@
+choco install -y vscodium github-desktop python3 7zip vlc git curl vim brave virtualbox wsl2 signal qbittorrent bitwarden steam discord
+
+
+choco install -y vscodium github-desktop python3 7zip vlc git curl vim wsl2 signal qbittorrent steam discord
