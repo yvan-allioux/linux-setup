82a4e635ca0e0680b666c9f4dc4dac5ad34963a0
yvan-allioux <yvan.allioux.pro@gmail.com>
2024-02-29 10:45:28 +0100

    Update compose wsl_install.sh

--- wsl_install.sh
+++ wsl_install.sh
@@ -36,7 +36,7 @@ sudo su - ${USER}
 echo "---docker compose---"
 
 #Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.6/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 echo "---zsh---"
