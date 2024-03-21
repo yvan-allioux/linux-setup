7dac7503ad5c9dc8748b0c37c0367a8f7808e2b4
yvan-allioux <yvan.allioux.pro@gmail.com>
2024-03-21 17:27:54 +0100

    Update choco.ps1

--- choco.ps1
+++ choco.ps1
@@ -1,7 +1,7 @@
 choco install -y vscodium github-desktop python3 7zip vlc git curl vim brave virtualbox wsl2 signal qbittorrent bitwarden steam discord
 
-
 choco install -y vscodium github-desktop python3 7zip vlc git curl vim wsl2 signal qbittorrent steam discord
 
+choco install typst
 
-
+choco install dolphin
